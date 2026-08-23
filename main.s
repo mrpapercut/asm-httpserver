00b4a3eed43bb383f38b4882885a27166b098558
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2026-08-23 13:34:14 +0200

    Challenge 'socket'

--- main.s
+++ main.s
@@ -6,5 +6,16 @@
 .type _start, @function
 
 _start:
+    call create_socket
+
     mov rdi, 0
     call exit
+
+create_socket:
+    mov rax, 41 # 'socket' syscall
+    mov rdi, 2  # family -> AF_INET -> IP protocol family
+    mov rsi, 1  # type -> SOCK_STREAM
+    mov rdx, 0  # protocol
+    syscall
+
+    ret
